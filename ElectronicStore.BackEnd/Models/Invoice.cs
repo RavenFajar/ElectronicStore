2d6a2007ba91aef335fa3df73a5ef98c623f83d7
Raven Fajar <ravenfajar2502@gmail.com>
2025-08-12 22:22:53 +0700

    Initial Database Update

---
+++ ElectronicStore.BackEnd/Models/Invoice.cs
@@ -0,0 +1,12 @@
+namespace ElectronicStore.BackEnd.Models;
+
+public sealed class Invoice
+{
+    public Guid Id { get; set; }
+    public DateTimeOffset InvoiceDate { get; set; }
+    public int LabourPrice { get; set; }
+    public Int64 GrandPrice { get; set; }
+    public List<InvoiceDetails> InvoiceDetails { get; set; } = new();
+    public byte[]? RowVersion { get; set; }
+
+}
